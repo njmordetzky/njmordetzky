57537f9b0608bfcd9e55fe479c010ad4526b75cd
nathaniel mordetzky <njmordetzky@gmail.com>
2016-09-13 01:46:25 -0700

    first commit for my site

---
+++ js/nathanielmordetzky.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	$('.skilBox').hover(function (e) {
+
+		$(this).addClass('animate');
+	});
+
+    $('.skilBox').on(
+    	'webkitAnimationEnd \
+    	mozAnimationEnd \
+    	MSAnimationEnd \
+    	oanimationend \
+    	animationend',
+		function (e) {
+			$(this).removeClass('animate');
+		});
+});
+
+
+
+// $('.skilBox').hover(
+//   function() {
+//     $(this).removeClass('animate');
+// }, 
+//   function() {
+//     $(this).addClass('animate'); 
+//   }
+// );
+
+// $('.skilBox').hover(
+//   function() {
+//     $(this).addClass('animate'); 
+//   }
+// );
+
+// $( ".skilBox" ).off( "mouseenter mouseleave" );
+
+// setTimeout(function(){
+//   $('.skilBox').hover(function() {
+//     $(this).removeClass('animate');
+//   }); 
+// }, 2000);
+
+// $( "td" ).hover(
+//   function() {
+//     $( this ).addClass( "hover" );
+//   }, function() {
+//     $( this ).removeClass( "hover" );
+//   }
+// );
+
+// setTimeout(function() {
+//   $('skilBox').removeClass('animate');
+// }, 1000); // The length of your animation
